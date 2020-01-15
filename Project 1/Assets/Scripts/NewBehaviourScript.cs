46bc0f3c665ec9383fd336b963c983b22b96769e
Jacob Winder <fyuryusgamer@gmail.com>
2020-01-15 09:53:24 -0700

    ???

--- Project 1/Assets/Scripts/NewBehaviourScript.cs
+++ Project 1/Assets/Scripts/NewBehaviourScript.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewClass : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print(message: "Hello World");
+        print("Hello world");
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(message: "Hello World");
+        print("Hello World");
     }
 }
