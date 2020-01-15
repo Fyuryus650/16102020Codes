2d24a26dce372ba7f7099d490cb521f78824a8e9
Jacob Winder <fyuryusgamer@gmail.com>
2020-01-15 10:50:18 -0700

    Var,names, and the like

---
+++ Project 1/Assets/Scripts/NewClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gamObj;
+    
+    public void Start()
+    {
+        print(nameString);
+        print(intNum);
+        print(floatNum); 
+        print(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
