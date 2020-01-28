b47da79cf219e620fa967eaa461ba4f24580d7e9
Jacob Winder <fyuryusgamer@gmail.com>
2020-01-27 19:40:41 -0700

    added exit event
    began adding exit event

--- Project 1/Assets/Scripts/TriggerEvents.cs
+++ Project 1/Assets/Scripts/TriggerEvents.cs
@@ -1,12 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerExitEvent;
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
     }
 }
