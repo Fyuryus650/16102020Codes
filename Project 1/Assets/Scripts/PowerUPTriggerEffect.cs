e0ba5553f4e274a0a332a7fdc32a70c8ec65194d
Jacob Winder <fyuryusgamer@gmail.com>
2020-01-29 09:22:03 -0700

    Adding Effects
    Attempted to add a text box to the "power-up". Didnt Work.

---
+++ Project 1/Assets/Scripts/PowerUPTriggerEffect.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PowerUPTriggerEffect : MonoBehaviour
+{
+   public UnityEvent entryEvent;
+   public UnityEvent exitEvent;
+   public UnityEvent stayEvent;
+   private void OnTriggerEnter(Collider other)
+   {
+      entryEvent.Invoke();
+   }
+
+   private void OnTriggerStay(Collider other)
+   {
+      stayEvent.Invoke();
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      exitEvent.Invoke();
+   }
+}
