a787131c375d1d77c211841c493f6961accfc8f9
Jacob Winder <fyuryusgamer@gmail.com>
2020-01-29 11:18:55 -0700

    playing with mouse functions
    MOuse functions testing in class

---
+++ Project 1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+    public UnityEvent mouseUpEvent;
+    public UnityEvent mouseDragEvent, mouseEnterEvent, onMouseOverEvent, onMouseExitEvent;
+    private void OnMouseDown()
+    {
+        mouseDownEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        onMouseOverEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        onMouseExitEvent.Invoke();
+    }
+}
