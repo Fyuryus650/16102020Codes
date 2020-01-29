6cee06e2259c993de6ddb94c69ef81b2f71ae24c
Jacob Winder <fyuryusgamer@gmail.com>
2020-01-29 10:48:02 -0700

    15 minute scripting task
    at the beginning of class we were given criteria for a blank script. Coded and added to a project.

---
+++ Project 1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public int number;
+    public float otherNumber;
+    public UnityEvent enterEvent;
+    public UnityEvent stayEvent;
+    public UnityEvent exitEvent;
+    public string stringName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       enterEvent.Invoke();
+       print(number);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+       stayEvent.Invoke();
+       print(otherNumber);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+      exitEvent.Invoke();
+      print(stringName);
+    }
+}
