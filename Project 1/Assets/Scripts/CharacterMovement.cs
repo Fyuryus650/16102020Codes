10e09ddfd2bca31d5c67c298b214a166af6bc1d3
Jacob Winder <fyuryusgamer@gmail.com>
2020-02-03 11:20:44 -0700

    Character Controller
    Created a character controller

---
+++ Project 1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Timers;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Vertical") * speed;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        positionDirection.z = Input.GetAxis("Horizontal") * speed;
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
