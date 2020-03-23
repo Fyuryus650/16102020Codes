d7c4c14b2b106307fe53455562477521ab670a1b
Jacob Winder <fyuryusgamer@gmail.com>
2020-03-23 11:03:03 -0600

    Switches
    Learned Switches and enums

---
+++ Project 1/Assets/Scripts/EnumsSwitches.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnumsSwitches : MonoBehaviour
+{
+    public UnityEvent startingEvent, playingEvent, inStoreEvent, pausingEvent, endingEvent;
+    public UnityEvent idleEvent, walkEvent, runEvent, jumpEvent;
+    public enum GameStates
+    {
+        Starting,
+        Playing,
+        InStore,
+        Pausing,
+        Ending,
+    }
+
+    public GameStates currentGameState;
+
+    public enum PlayerStates
+    {
+        Idle,
+        Walk,
+        Run,
+        Jump
+    }
+
+    public PlayerStates currentPlayerState;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        switch (currentPlayerState)
+        {
+            case PlayerStates.Idle:
+                idleEvent.Invoke();
+                break;
+            case PlayerStates.Walk:
+                walkEvent.Invoke();
+                break;
+            case PlayerStates.Run: 
+                runEvent.Invoke();
+                break;
+            case PlayerStates.Jump:
+                jumpEvent.Invoke();
+                break;
+            default:
+                print("i am any");
+                break;
+        }
+
+        switch (currentGameState)
+        {
+            case GameStates.Starting:
+                startingEvent.Invoke();
+                break;
+            case GameStates.Playing:
+                playingEvent.Invoke();
+                break;
+            case GameStates.InStore:
+                inStoreEvent.Invoke();
+                break;
+            case GameStates.Pausing:
+                pausingEvent.Invoke();
+                break;
+            case GameStates.Ending:
+                endingEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
